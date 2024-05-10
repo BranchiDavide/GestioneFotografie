8bdc3873dad1115b408e61faae6adb39b90420d0
Davide Branchi <davide.branchi@samtrevano.ch>
2024-05-10 14:31:56 +0200

    Date and time autofilling in upload form from img EXIF data

--- public/js/imagePreview.js
+++ public/js/imagePreview.js
@@ -1,12 +1,29 @@
-document.getElementById("formFile").addEventListener("change", (e) => {
+const previewImage = document.getElementById("previewImage");
+const dataOraInput = document.getElementsByName("data-ora")[0];
+document.getElementById("formFile").addEventListener("change", async(e) => {
     let file = e.target.files[0];
     if (file) {
         const reader = new FileReader();
-        reader.onload = function (e) {
-            const previewImage = document.getElementById("previewImage");
+        reader.onload = async function (e) {
             previewImage.src = e.target.result;
             document.getElementsByClassName("item-picture")[0].style.display = "block";
+
+            try{
+                // Lettura dati EXIF dell'immagine per ricavare data e ora
+                const exifData = piexif.load(previewImage.src);
+                const dateTime = exifData["0th"]["306"];
+                if(dateTime){
+                    dataOraInput.value = formatDateInput(dateTime);
+                }
+            }catch{}
         };
         reader.readAsDataURL(file);
     }
 });
+function formatDateInput(dateString) {
+    const [date, time] = dateString.split(' ');
+    const [year, month, day] = date.split(':');
+    const [hour, minute, second] = time.split(':');
+    const formattedDate = `${year}-${month}-${day}T${hour}:${minute}`;
+    return formattedDate;
+}
